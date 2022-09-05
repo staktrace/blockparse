0e1e3bf48d4411e365c7a74b1a329f5c7f5dab3c
Kartikaya Gupta <kats@persona.staktrace.com>
2022-09-05 16:00:12 -0400

    Rename to reflect expanding scope

--- examples/parse_files.rs
+++ examples/parse_files.rs
@@ -8,7 +8,7 @@ fn main() {
         let mut bytes = Vec::new();
         file.read_to_end(&mut bytes).unwrap();
         println!("Parsing {:?}...", arg);
-        match blockparse::parse::parse_blockfile(&bytes) {
+        match blocktastic::parse::parse_blockfile(&bytes) {
             Ok(blocks) => {
                 println!("Parsed {} blocks", blocks.len());
                 blocks.iter().for_each(|b| println!("{}", b));
