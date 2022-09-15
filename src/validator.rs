fac796659f11b69db1581daf82a76be4621fb728
Kartikaya Gupta <kats@trevize.staktrace.com>
2022-09-14 23:04:32 -0400

    Implement the archiving step

--- src/validator.rs
+++ src/validator.rs
@@ -8,6 +8,7 @@ use std::time::SystemTime;
 
 const MAX_SUPPORTED_BLOCK_VERSION: u32 = 4;
 const TWO_HOURS_IN_SECONDS: u64 = 2 * 60 * 60;
+const MAX_ACTIVE_HEIGHT: usize = 144; // One day's worth of blocks
 
 /// A state machine to validate blocks as they are received. This structure accepts
 /// blocks one at a time, and checks to see if it is valid, updating internal state
@@ -16,7 +17,17 @@ const TWO_HOURS_IN_SECONDS: u64 = 2 * 60 * 60;
 /// discard abandoned chains if there is a clear "winner" chain.
 #[derive(Default)]
 pub struct BlockValidator {
+    /// Map from block id to block height for archived blocks. Genesis block is height 0.
+    /// Archived blocks are always a linear chain; branches will have been pruned away.
     archived_blocks: HashMap<Hash, usize>,
+    /// Map from hash to block and associated metadata for active blocks. Active blocks
+    /// are recent blocks that have been validated and connected to the chain. Active
+    /// blocks form a tree rooted at the most recent archived block. Generally the longest
+    /// path in the tree is the one with the most proof-of-work, and therefore the
+    /// canonical blockchain, but that may change. Once the longest path in the active
+    /// block tree is longer than MAX_ACTIVE_HEIGHT, the oldest active blocks on that
+    /// path are archived and shorter branches emanating from those archived blocks
+    /// get pruned away.
     active_blocks: HashMap<Hash, ActiveBlock>,
 }
 
@@ -84,9 +95,71 @@ impl BlockValidator {
         info!("Adding block {} to chain at height {}", hash, height);
         self.active_blocks.insert(hash, active_block);
 
-        // TODO: archive old active blocks and prune tree
+        if height - self.archived_blocks.len() >= MAX_ACTIVE_HEIGHT {
+            self.archive_old_blocks(&hash);
+        }
 
         ValidationResult::Valid(hash)
+    }
+
+    fn archive_old_blocks(&mut self, leaf_hash: &Hash) {
+        let mut iter_hash = *leaf_hash;
+        let mut active_root = iter_hash;
+        // Walk up following the parent links such that active_root and iter_hash are
+        // separated by the new archiving boundary. active_root will remain active and
+        // iter_hash (plus any active ancestors) will get archived.
+        for _i in 0..MAX_ACTIVE_HEIGHT {
+            active_root = iter_hash;
+            iter_hash = self.active_blocks.get(&iter_hash).unwrap().block.header.prev_block_hash;
+        }
+
+        // Archive iter_hash and active ancestors until there are no more active ancestors.
+        loop {
+            iter_hash = match self.active_blocks.remove(&iter_hash) {
+                Some(removed) => {
+                    info!("Archiving {} with height {}", &iter_hash, removed.height);
+                    self.archived_blocks.insert(iter_hash, removed.height);
+                    removed.block.header.prev_block_hash
+                }
+                None => break,
+            };
+        }
+
+        // Next we want to prune away the dead branches (i.e. any node where following the
+        // parent links takes you to an archived node without passing through active_root.
+        // We implement this by making a new replacement map, retained_active_blocks, and
+        // moving nodes we want to keep into there. Since we seed retained_active_blocks with
+        // active_root, the "nodes we want to keep" are simply the ones where walking the
+        // parent links takes you to a node already in retained_active_blocks. Everything else
+        // is discarded.
+
+        let mut retained_active_blocks = HashMap::new();
+        retained_active_blocks.insert(active_root, self.active_blocks.remove(&active_root).unwrap());
+        // seeding done, now walk the rest of the active blocks and keep anything in the
+        // subtree rooted at active_root.
+        let active_block_hashes = self.active_blocks.keys().copied().collect::<Vec<Hash>>();
+        for hash in active_block_hashes {
+            let root = self.get_active_root(&hash);
+            if retained_active_blocks.contains_key(&root) {
+                retained_active_blocks.insert(hash, self.active_blocks.remove(&hash).unwrap());
+            }
+        }
+
+        // Pruning done, now swap our final result back in
+        std::mem::swap(&mut self.active_blocks, &mut retained_active_blocks);
+    }
+
+    // Returns the leafmost node that is an ancestor of the given hash but that is NOT in
+    // the self.active_blocks set.
+    fn get_active_root(&self, hash: &Hash) -> Hash {
+        let mut root = *hash;
+        loop {
+            root = match self.active_blocks.get(&root) {
+                Some(parent) => parent.block.header.prev_block_hash,
+                None => break,
+            };
+        }
+        root
     }
 
     fn validate_block(&mut self, block: &Block, height: usize) -> Result<(), BlockValidationError> {
